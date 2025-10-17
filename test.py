3ca6f42a093eb3adaf107e967d4a2a1362e3b3e9
saml1998 <slavudya1998@gmail.com>
2025-10-17 14:55:37 -0500

    Step

--- test.py
+++ test.py
@@ -1,7 +1,43 @@
-// ---- Dropdown States ----
-const [platforms, setPlatforms] = useState<any[]>([]);
-const [practices, setPractices] = useState<any[]>([]);
-const [offerings, setOfferings] = useState<any[]>([]);
+// Load Platforms when page mounts
+useEffect(() => {
+  fetch("http://127.0.0.1:8000/mapping/platforms")
+    .then((res) => res.json())
+    .then((data) => {
+      setPlatforms(data);
+      console.log("Loaded Platforms:", data);
+    })
+    .catch((err) => console.error("Error fetching platforms:", err));
+}, []);
 
-const [selectedPlatform, setSelectedPlatform] = useState('');
-const [selectedPractice, setSelectedPractice] = useState('');
+// Load Practices when Platform selected
+useEffect(() => {
+  if (selectedPlatform) {
+    fetch(`http://127.0.0.1:8000/mapping/practices/${selectedPlatform}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPractices(data);
+        console.log("Loaded Practices:", data);
+      })
+      .catch((err) => console.error("Error fetching practices:", err));
+  } else {
+    setPractices([]);
+    setOfferings([]);
+  }
+}, [selectedPlatform]);
+
+// Load Offerings when Practice selected
+useEffect(() => {
+  if (selectedPlatform && selectedPractice) {
+    fetch(
+      `http://127.0.0.1:8000/mapping/offerings/${selectedPlatform}/${selectedPractice}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setOfferings(data);
+        console.log("Loaded Offerings:", data);
+      })
+      .catch((err) => console.error("Error fetching offerings:", err));
+  } else {
+    setOfferings([]);
+  }
+}, [selectedPractice]);
