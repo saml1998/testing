0c7f8733dedf14b07a5d93bf504d5925c405c357
saml1998 <slavudya1998@gmail.com>
2025-10-22 14:43:08 -0500

    s

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-INSERT INTO practice (practice_code, practice_name, platform_code) VALUES
-('OPM_009', 'App Modernization & Development', 'OPM_002'),
-('OPM_010', 'Cloud & Infrastructure Operations', 'OPM_002'),
-('OPM_011', 'Data Engineering & Analytics', 'OPM_002'),
-('OPM_012', 'Technology Managed Services', 'OPM_002'),
-('OPM_013', 'BSEO Managed Services', 'OPM_001'),
-('OPM_014', 'Sector Operations', 'OPM_001'),
-('OPM_015', 'PNC Operations', 'OPM_001'),
-('OPM_016', 'Tech Powered By', 'OPM_001'),
-('OPM_017', 'Field Service Operations', 'OPM_001');
 
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base
+
+class Practice(Base):
+    __tablename__ = "practice"
+
+    id = Column(Integer, primary_key=True, index=True)
+    practice_code = Column(String, unique=True, index=True)
+    practice_name = Column(String)
+    platform_code = Column(String, ForeignKey("platform.platform_code"))
