18ad8d2d296e5c6381315604fc325365f1aac876
saml1998 <slavudya1998@gmail.com>
2025-10-23 15:26:51 -0500

    run

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 SELECT 
     p.platform_name, 
-    pr.practice_name
-FROM public.practice_catalog pr
+    pr.practice_name, 
+    o.offering_name
+FROM public.offering_catalog o
+JOIN public.practice_catalog pr 
+    ON o.practice_id = pr.practice_id
 JOIN public.platform_catalog p 
     ON pr.platform_id = p.platform_id;
+
 
 
 
