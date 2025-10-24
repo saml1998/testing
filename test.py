8bd71573eb3ef2399f52d948054d8774161408e9
saml1998 <slavudya1998@gmail.com>
2025-10-24 11:40:35 -0500

    init

--- test.py
+++ test.py
@@ -1,20 +1,9 @@
-from fastapi import APIRouter, Query
-from app.utils.catalogs_ado import CatalogsADO
+from fastapi import FastAPI
+from app.routes.catalogs import router as catalogs_router
 
-router = APIRouter(prefix="/catalogs", tags=["catalogs"])
-ado = CatalogsADO()
+app = FastAPI()
+app.include_router(catalogs_router)
 
-@router.get("/platforms")
-async def get_platforms():
-    return await ado.get_platforms()
-
-@router.get("/practices")
-async def get_practices(platform_id: int = Query(..., gt=0)):
-    return await ado.get_practices(platform_id)
-
-@router.get("/offerings")
-async def get_offerings(practice_id: int = Query(..., gt=0)):
-    return await ado.get_offerings(practice_id)
 
 
 
