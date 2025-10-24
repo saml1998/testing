627e9cc03a37009bc4a447793bf664446f082936
saml1998 <slavudya1998@gmail.com>
2025-10-24 10:54:18 -0500

    post

--- test.py
+++ test.py
@@ -1,20 +1,25 @@
-import psycopg2
+-- In postgres DB
+CREATE TABLE IF NOT EXISTS public.platform_catalog (
+  platform_id   SERIAL PRIMARY KEY,
+  platform_code TEXT UNIQUE NOT NULL,
+  platform_name TEXT NOT NULL
+);
 
-try:
-    conn = psycopg2.connect(
-        host="u23jghvfsddp127.postgres.database.azure.com",
-        dbname="opco_db",
-        user="adminuser@u23jghvfsddp127",
-        password="3Jt*y38++{#mF5mpxEuf*6kMW->",
-        port="5432",
-        sslmode="require"
-    )
+CREATE TABLE IF NOT EXISTS public.practice_catalog (
+  practice_id   SERIAL PRIMARY KEY,
+  platform_id   INT NOT NULL REFERENCES public.platform_catalog(platform_id) ON DELETE CASCADE,
+  practice_code TEXT NOT NULL,
+  practice_name TEXT NOT NULL,
+  UNIQUE (platform_id, practice_code)
+);
 
-    print("✅ Connected successfully!")   # inside try
-    conn.close()                          # close connection
-
-except Exception as e:
-    print("❌ Connection failed:", e)
+CREATE TABLE IF NOT EXISTS public.offering_catalog (
+  offering_id   SERIAL PRIMARY KEY,
+  practice_id   INT NOT NULL REFERENCES public.practice_catalog(practice_id) ON DELETE CASCADE,
+  offering_code TEXT NOT NULL,
+  offering_name TEXT NOT NULL,
+  UNIQUE (practice_id, offering_code)
+);
 
 
 
