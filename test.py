cdea02d7b4da637a8ade84c710a03d84708da0f7
saml1998 <slavudya1998@gmail.com>
2025-10-24 12:37:06 -0500

    bac

--- test.py
+++ test.py
@@ -1,21 +1,4 @@
-from fastapi import APIRouter
-from typing import List, Dict, Any
-from app.utils.catalogs_ado import CatalogsADO
-
-router = APIRouter(prefix="/catalogs", tags=["Catalogs"])
-catalogs_ado = CatalogsADO()
-
-@router.get("/platforms")
-async def get_platforms() -> List[Dict[str, Any]]:
-    return await catalogs_ado.get_platforms()
-
-@router.get("/practices/{platform_id}")
-async def get_practices(platform_id: int) -> List[Dict[str, Any]]:
-    return await catalogs_ado.get_practices(platform_id)
-
-@router.get("/offerings/{practice_id}")
-async def get_offerings(practice_id: int) -> List[Dict[str, Any]]:
-    return await catalogs_ado.get_offerings(practice_id)
+uvicorn backend.app.__init__:app --reload
 
 
 
